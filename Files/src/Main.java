c1a797f8a29824ee0b114f572539f9f8342a1be7
991o2o9 <1kkro123@gmail.com>
2026-02-07 22:12:41 +0600

    File

---
+++ Files/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args){
+    // Writer
+    String filePath = "text.txt";
+    String textContent = """
+            Roses are blue
+            Violets are blue
+            BOOTY BOOTY BOOTY
+            ROCKING EVERYWHERE
+            """;
+
+    try(FileWriter writer = new FileWriter(filePath);) {
+      writer.write(textContent);
+      System.out.println("File has been written");
+    }
+    catch (FileNotFoundException e){
+      System.out.println("Could not locate file location");
+    }
+    catch (IOException e){
+      System.out.println("Could not write a file: " + e.getMessage());
+    }
+
+
+  // Reader
+  String filePathRe = "text.txt";
+
+
+  try(BufferedReader reader = new BufferedReader(new FileReader(filePathRe))){
+
+    String line;
+    while ((line = reader.readLine()) != null){
+      System.out.println(line);
+    }
+  }
+  catch(FileNotFoundException e){
+    System.out.println("Could not locate file location"+ e.getMessage());
+  }
+  catch(IOException e){
+    System.out.println("Something went wrong");
+  }
+  }
+}
